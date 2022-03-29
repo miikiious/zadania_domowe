01f9756a6dc407ec655c9f482781fca89edcafa5
Mikołaj Biszczak <mikolaj.biszczak@gmail.com>
2022-03-29 18:27:11 +0200

    bangla

---
+++ zadanka_domowe/zad_1.7.py
@@ -0,0 +1,6 @@
+przedmiot=input("podaj co chcesz kupic: ")
+cena=float(input("podaj cenę: "))
+ilosc=float(input("podaj ilość którą chcesz kupić: "))
+
+print(f"za {przedmiot} do zapłaty masz {(cena*ilosc):.2f}zł")
+
