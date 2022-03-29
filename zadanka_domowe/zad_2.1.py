a7f168c08316f4e0f5670aef35e49c7628fbba6d
Mikołaj Biszczak <mikolaj.biszczak@gmail.com>
2022-03-29 18:45:01 +0200

    bangla

--- zadanka_domowe/zad_2.1.py
+++ zadanka_domowe/zad_2.1.py
@@ -1,8 +1,9 @@
 import random
-liczb_a=random.randint(0,99)
-liczb_b=random.randint(0,99)
-suma=int(input(f"podaj sumę {liczb_a}+{liczb_b}= "))
-while suma != liczb_a+liczb_b:
+
+liczb_a = random.randint(0, 99)
+liczb_b = random.randint(0, 99)
+suma = int(input(f"podaj sumę {liczb_a}+{liczb_b}= "))
+while suma != liczb_a + liczb_b:
     print(f"postaraj się lepiej {liczb_a}+{liczb_b}!={suma}")
     suma = int(input(f"spróbuj jeszcze raz: "))
 print("zgadłeś!")
